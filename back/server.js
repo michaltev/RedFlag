0526509270c2ae44912734f62d98eff96888e0f8
michaltev <michaltev@gmail.com>
2020-05-18 12:53:30 +0300

    users controller added
    now we can register+signin

--- back/server.js
+++ back/server.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const cors = require('cors')
+const knex = require('knex')
 
-/*const db = knex({
+const usersController = require('./controllers/usersController');
+
+const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
     user : 'postgres',
     password : 'admin',
-    database : 'festivematch'
+    database : 'redflag'
   }
-});*/
+});
 
 var app = express()
 
@@ -20,6 +23,12 @@ app.get('/', (req, res) => {
 	console.log('getting');
 	res.json('getting');
 });
+
+app.post('/signin', (req, res) => { usersController.handleSignin(req, res, db) });
+
+app.post('/register', (req, res) => { usersController.handleRegister(req, res, db) });
+
+app.get('/profile/:id', (req, res) => { usersController.getUserProfile(req, res, db) });
 
 app.listen(3000, () => {
 	console.log('app is running');
